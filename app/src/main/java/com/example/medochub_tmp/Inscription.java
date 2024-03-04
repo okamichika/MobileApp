a5ee14011019907f442562a65ddf7adbdc62b877
Romain Mi <romainmivi@gmail.com>
2024-03-04 17:32:07 +0100

    04-03-2024

--- app/src/main/java/com/example/medochub_tmp/Inscription.java
+++ app/src/main/java/com/example/medochub_tmp/Inscription.java
@@ -2,7 +2,12 @@ package com.example.medochub_tmp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Inscription extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class Inscription extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription);
+
+
+        TextView connexion = (TextView) findViewById(R.id.haveaccount);
+
+        connexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("MainActivity", "onClick: Bouton connection_id cliqué!!!");
+
+                Intent intent = new Intent(getApplicationContext(), authentification.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/medochub_tmp/authentification.java
+++ app/src/main/java/com/example/medochub_tmp/authentification.java
@@ -2,7 +2,11 @@ package com.example.medochub_tmp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class authentification extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class authentification extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentification);
+
+        TextView createaccount = (TextView) findViewById(R.id.nothaveaccount);
+
+        createaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("MainActivity", "onClick: Bouton connection_id cliqué!!!");
+
+                Intent intent = new Intent(getApplicationContext(), Inscription.class);
+                startActivity(intent);
+            }
+        });
     }
 }
